376dd0ae408f5d7d269e3d7728eddecb28f3f5a3
Damien Murtagh <MurtaghD@mbbc.qld.edu.au>
2019-03-29 15:02:29 +1000

    Euclids tiles part 2

--- Ch1-3 Euclid's Tiles.py
+++ Ch1-3 Euclid's Tiles.py
@@ -1,3 +1,9 @@
+from tkinter import *
+# CONSTANTS
+WIDTH = 800
+HEIGHT = 600
+
+
 # FUNCTIONS
 # calcuate tile size
 def calculate_tile_size(long_side, short_side):
@@ -10,6 +16,24 @@ def calculate_tile_size(long_side, short_side):
     return long_side
 
 # display the floor
+def floor_display(room_long, room_short, tile, floor_canvas):
+    # calcualte variables
+    number_of_tiles_X = room_long // tile
+    number_of_tiles_Y = room_short // tile
+    x1 = 0
+    y1 = 0
+
+    # generate tiles on canvas
+    for count_y_tiles in range(0, number_of_tiles_Y):
+        for count_x_tiles in range(0,number_of_tiles_X):
+            x2 = x1 + tile
+            y2 = y1 + tile
+            floor_canvas.create_rectangle(x1,y1,x2,y2, fill="Pink")
+            x1 = x2
+        y1 = y1 + tile
+        x1 = 0
+
+
 
 
 # display tile size
@@ -17,7 +41,16 @@ def calculate_tile_size(long_side, short_side):
 
 
 # ---- MAIN PROGRAM ----
+# Initialise window
+window = Tk()
+window.title("Euclid's Tiles")
+canvas = Canvas(window, width=WIDTH, height=HEIGHT)
+canvas.pack()
+
 room_length = int(input("Room length: "))
 room_width = int(input("Room width: "))
 tile_size = calculate_tile_size(room_length,room_width)
-print(tile_size)
+#print(tile_size)
+floor_display(room_length,room_width,tile_size,canvas)
+
+window.mainloop()
